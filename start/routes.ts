785fd6b6132c9fee5ba56d81f30d57399f0672ce
Muhammad Hannan <mdhannan.info@gmail.com>
2024-07-23 13:11:45 +0600

    Change route method

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,7 @@ import router from '@adonisjs/core/services/router'
 router.get('', '#controllers/homes_controller.index').as('home.page')
 router.get('about', '#controllers/homes_controller.about').as('about.page')
 router.get('services', '#controllers/homes_controller.services').as('services.page')
-router.get('contact', '#controllers/homes_controller.contact').as('contact.page')
+router.any('contact', '#controllers/homes_controller.contact').as('contact.page')
 
 router.get('blogs', '#controllers/blogs_controller.index').as('blog.index')
 
